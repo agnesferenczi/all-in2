57c2694bad8784594ec6e088d9bd9a9d882325c1
agnesferenczi <65777475+agnesferenczi@users.noreply.github.com>
2020-11-24 22:15:01 +0100

    javítás - javascript

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const men_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a')
 const header = document.querySelector('.header.container');
 let galleryImages = document.querySelectorAll(".gallery-image");
 let getLatestOpenedImg;
-let windowWidth = window.innerWidth;
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active');
