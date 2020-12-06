3fee1819fd9c9e628a51ff8c84857e46e73e8e16
Krisztian Szabo <thenonameguy24@gmail.com>
2020-12-06 16:18:47 +0100

    Add lightgallery and basic example

--- gallery.js
+++ gallery.js
@@ -1,3 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("loaded");
+$(document).ready(function () {
+    let alpinTechnique = $('.alpin-technika');
+    alpinTechnique.click(function (_e) {
+        alpinTechnique.lightGallery({
+            dynamic: true,
+            dynamicEl: [{
+                "src": 'img/img3.jpg'
+            },
+            {
+                "src": 'img/img4.jpg'
+            }
+            // ,
+            //             {
+            //     'src': '../static/img/2.jpg',
+            //     'thumb': '../static/img/thumb-2.jpg',
+            //     'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
+            // }, {
+            //     'src': '../static/img/3.jpg',
+            //     'thumb': '../static/img/thumb-3.jpg',
+            //     'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
+            // }
+                       ]
+        });
+        return false;
+    });
 });
