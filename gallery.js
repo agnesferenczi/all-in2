57e8e472583b1d4e746bb0cd3dca494f88d16658
Krisztian Szabo <thenonameguy24@gmail.com>
2020-12-06 16:26:06 +0100

    Generalize lightgallery

--- gallery.js
+++ gallery.js
@@ -1,26 +1,19 @@
 $(document).ready(function () {
-    let alpinTechnique = $('.alpin-technika');
-    alpinTechnique.click(function (_e) {
-        alpinTechnique.lightGallery({
-            dynamic: true,
-            dynamicEl: [{
-                "src": 'img/img3.jpg'
-            },
-            {
-                "src": 'img/img4.jpg'
-            }
-            // ,
-            //             {
-            //     'src': '../static/img/2.jpg',
-            //     'thumb': '../static/img/thumb-2.jpg',
-            //     'subHtml': "<h4>Bowness Bay</h4><p>A beautiful Sunrise this morning taken En-route to Keswick not one as planned but I'm extremely happy I was passing the right place at the right time....</p>"
-            // }, {
-            //     'src': '../static/img/3.jpg',
-            //     'thumb': '../static/img/thumb-3.jpg',
-            //     'subHtml': "<h4>Coniston Calmness</h4><p>Beautiful morning</p>"
-            // }
-                       ]
+    let jobsToImages = {
+        '.alpin-technika':  [{src: 'img/img3.jpg'},
+                             {src: 'img/img4.jpg'}],
+        '.epitoipari-munkak': [{src: 'img/img5.jpg'},
+                               {src: 'img/img6.jpg'}]
+    };
+
+    for (const [selector, images] of Object.entries(jobsToImages)) {
+        let elem = $(selector);
+        elem.click(function (_e) {
+            elem.lightGallery({
+                dynamic: true,
+                dynamicEl: images,
+            });
+            return false;
         });
-        return false;
-    });
+    }
 });
