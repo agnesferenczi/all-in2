22fec5ccfd0ac3040305479cec3b172b31d62214
agnesferenczi <65777475+agnesferenczi@users.noreply.github.com>
2021-03-02 19:54:45 +0100

    fadein scroll + háttér(fekete) + favicon

--- fadein.js
+++ fadein.js
@@ -1,11 +1,3 @@
-const checkpoint = 200;
-
-window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    if (currentScroll <= checkpoint) {
-        opacity = 0 - currentScroll / checkpoint;
-    } else {
-        opacity = 1;
-    }
-    document.querySelector("#bemutat .call").style.opacity = opacity;
-});
+< script >
+    AOS.init(); <
+/script>
